105ac14c7d603e4cc674de8013c0929858078afe
twzkraus <twzkraus@gmail.com>
2020-09-11 17:36:22 -0700

    cookieParser functional. Working on auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,22 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  console.log('inside of createSession');
+  models.Sessions.get({hash: req.cookies.shortlyid})
+    // valid id: get will return an object
+    .then((sessionData) => {
+      // not valid: get will return undefined
+      if (!sessionData) {
+        console.log('no session data was found');
+        let sessionHash = models.Sessions.create().then( data => { return data.hash; } );
+        req.session = { shortlyid: sessionHash };
+      } else {
+        console.log('session data was found');
+        // sessionData includes user id, hash, primary id
+        // use user id to log that person in
+      }
+    });
+  next();
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,7 +1,20 @@
 const parseCookies = (req, res, next) => {
   // parses cookies and assigns an object of key-value pairs to a session property on the request
 
-  // view all cookies on
+  // view all cookies on req object's header
+  // turn them into an object (from string)--key is prior to '=', value followed by ';'
+  if (req.headers.cookie !== undefined) {
+
+    let cookieArray = req.headers.cookie.split('; ');
+    let cookieObj = {};
+    cookieArray.forEach(element => {
+      let bothPieces = element.split('=');
+      cookieObj[bothPieces[0]] = bothPieces[1];
+    });
+    // put them onto the request
+    req.cookies = cookieObj;
+  }
+  next();
 
 };
 
